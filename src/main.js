48e84193804aef132d71f849fefa07c94ac5112d
Sarah <s.fawzy97@yahoo.com>
2024-10-19 12:53:11 +0300

    navbar

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import store from './store'
+import router from './router'
 
-createApp(App).mount('#app')
+import './style.css'
+
+
+createApp(App).use(router).mount('#app')
+App.use(store);
---
+++ src/store/index.js
@@ -0,0 +1,27 @@
+// src/store/index.js
+import { createStore } from 'vuex';
+
+const store = createStore({
+    state() {
+        return {
+            count: 0
+        };
+    },
+    mutations: {
+        increment(state) {
+            state.count++;
+        }
+    },
+    actions: {
+        increment({ commit }) {
+            commit('increment');
+        }
+    },
+    getters: {
+        getCount(state) {
+            return state.count;
+        }
+    }
+});
+
+export default store;
