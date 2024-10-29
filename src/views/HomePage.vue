1a09db1fde5e5ae0d050ed73a8e568b88a78f430
Sarah <s.fawzy97@yahoo.com>
2024-10-29 23:50:47 +0300

    .

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,27 +1,46 @@
 <template>
   
-<div class="homeBanner pt-28  pr-20">
-    <div class="w-2/5 text-left p-12 ">
+<div class="homeBanner md:pt-32 pt-9 md:pr-20 pr-10 pb-28 ">
+    <div class="w-6/12 text-left md:p-11 p-3">
         <span class="text-xs font-semibold">New arrival</span>
-        <h3 class="font-bold text-5xl text-primary-color">Discover Our New Collection</h3>
-        <p class="font-medium text-lg pt-3 pb-12">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
-   <span><router-link to="/" class="font-bold bg-primary-color">Buy Now</router-link></span>
+        <h3 class="font-bold lg:text-5xl sm:text-2xl text-primary-color">Discover Our<br> New Collection</h3>
+        <p class="font-medium lg:text-lg text-xs pt-2 md:pb-12 pb-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+   <span><router-link to="/" class="font-bold bg-primary-color md:p-3">Buy Now</router-link></span>
+    </div>
+</div>
+
+<div>
+    <div class="my-20">
+    <h5 class="text-3xl font-bold ">Browse The Range</h5>
+    <p class="text-xl  text-gray">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+    <div>
+        <div v-for="category in categories" :key="category.name">
+            {{ category }}
+        </div>
     </div>
 </div>
 </template>
 
-<script>
-export default {
+<script setup>
+    import { onMounted } from 'vue';
+import getCategories from '../composable/categories'
+const {categories,load}=getCategories()
+categories
+load()
+onMounted(()=>{
+    getCategories()
+})
 
-}
 </script>
 
 <style>
 
 .homeBanner{    
     background: url('../assets/banner.png') no-repeat;
-    background-size: contain;
-    height: 716px;
+    background-size: cover;
+    /* height: 716px; */
+    overflow: hidden;
     
 }
 .homeBanner div{
@@ -35,5 +54,12 @@ export default {
     color: #fff;
     margin-top: 10px;
     padding: 16px 36px;
+}
+@media (max-width:786px) {
+    .homeBanner a{
+        font-size:10px;
+        padding:6px 14px;
+
+    }
 }
 </style>
